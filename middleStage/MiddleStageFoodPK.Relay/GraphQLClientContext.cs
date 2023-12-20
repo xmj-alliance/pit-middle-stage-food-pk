f26d817364b54eddba2146a4237c5f8aa5c68c40
longyiszh <longyiszh@outlook.com>
2023-12-20 11:48:41 -0500

    add GraphQL context

---
+++ middleStage/MiddleStageFoodPK.Relay/GraphQLClientContext.cs
@@ -0,0 +1,40 @@
+﻿using GraphQL.Client.Http;
+using GraphQL.Client.Serializer.SystemTextJson;
+using System.Text.Json.Serialization;
+using System.Text.Json;
+
+namespace MiddleStageFoodPK.Relay;
+
+public class GraphQLClientContext : IGraphQLClientContext
+{
+    private readonly HttpClient httpClient;
+    private readonly Uri endPoint;
+
+    public GraphQLHttpClient Client { get; init; }
+
+    public GraphQLClientContext(
+        IHttpClientFactory factory
+    )
+    {
+        httpClient = factory.CreateClient("client");
+        endPoint = new Uri($"{httpClient.BaseAddress}/graphql");
+
+        Client = new GraphQLHttpClient(
+            new GraphQLHttpClientOptions
+            {
+                EndPoint = endPoint,
+                UseWebSocketForQueriesAndMutations = false
+            },
+            new SystemTextJsonSerializer(
+                new JsonSerializerOptions()
+                {
+                    Converters = {
+                        new JsonStringEnumConverter(new ConstantCaseJsonNamingPolicy(), false)
+                    }
+                }.SetupImmutableConverter()
+            ),
+            httpClient
+        );
+    }
+
+}
---
+++ middleStage/MiddleStageFoodPK.Relay/IGraphQLClientContext.cs
@@ -0,0 +1,8 @@
+﻿using GraphQL.Client.Http;
+
+namespace MiddleStageFoodPK.Relay;
+
+public interface IGraphQLClientContext
+{
+    GraphQLHttpClient Client { get; }
+}
